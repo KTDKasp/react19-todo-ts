2d8d93b2872caafe4828ee684694507c28a388af
KTDKasp <churroskasp@gmail.com>
2025-02-22 11:03:01 +0300

    feat: add tanstack router instead react-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), TanStackRouterVite({ autoCodeSplitting: true })],
   resolve: {
     alias: {
       src: '/src'
